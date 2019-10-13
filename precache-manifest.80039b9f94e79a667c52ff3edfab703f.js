4fa057a562344051777aac6e7b17a3cb2b4ac600
suilujic <julius.munene@students.jkuat.ac.ke>
2019-10-13 11:45:30 +0300

    Updates

---
+++ precache-manifest.80039b9f94e79a667c52ff3edfab703f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0f1f5e82183d15a9e98c3da53865e02",
+    "url": "/Alexanderlacuna/clone_app.git.io/index.html"
+  },
+  {
+    "revision": "8ead2f906bb81d0953bc",
+    "url": "/Alexanderlacuna/clone_app.git.io/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "e599b079ae71c504cd72",
+    "url": "/Alexanderlacuna/clone_app.git.io/static/css/main.cd563a17.chunk.css"
+  },
+  {
+    "revision": "8ead2f906bb81d0953bc",
+    "url": "/Alexanderlacuna/clone_app.git.io/static/js/2.aa4d2859.chunk.js"
+  },
+  {
+    "revision": "e599b079ae71c504cd72",
+    "url": "/Alexanderlacuna/clone_app.git.io/static/js/main.2a6dcde6.chunk.js"
+  },
+  {
+    "revision": "ba2f6f7e21010c6c3771",
+    "url": "/Alexanderlacuna/clone_app.git.io/static/js/runtime-main.47969e95.js"
+  }
+]);
